c611825e9bbdf893d7fc1541141517a67917aeb6
“Irving <Irving>
2020-08-17 01:14:28 -0500

    fix db config

--- db.js
+++ db.js
@@ -8,6 +8,8 @@ module.exports = connectToDatabase = () => {
     }
     console.log('=> using new database connection');
     return mongoose.connect(process.env.DB).then(db => {
-        isConnected = db.connections[0].readyState;
+        useUnifiedTopology: true
+        useNewUrlParser: true
+            //isConnected = db.connections[0].readyState;
     });
 };
